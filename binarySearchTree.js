a8b2397507f03e88d3a2a4afeadc3400d28f34b6
magn3tism <chapagainsakshyam@gmail.com>
2022-12-23 08:49:32 +0545

    Add instertion method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,35 +1,72 @@
 const Node = require("./Node");
 
 class BinarySearchTree {
-  buildTree(array, start, end) {
+  #root;
+
+  constructor(array) {
+    this.#root = this.#buildTree(array, 0, array.length - 1);
+  }
+
+  #buildTree(array, start, end) {
     if (start > end) return null;
 
     let mid = parseInt((start + end) / 2);
     const root = new Node(array[mid]);
 
-    root.left = this.buildTree(array, start, mid - 1);
+    root.left = this.#buildTree(array, start, mid - 1);
 
-    root.right = this.buildTree(array, mid + 1, end);
+    root.right = this.#buildTree(array, mid + 1, end);
 
     return root;
   }
 
-  prettyPrint(node, prefix = "", isLeft = true) {
+  #prettyPrint(node, prefix = "", isLeft = true) {
     if (node.right !== null) {
-      this.prettyPrint(
+      this.#prettyPrint(
         node.right,
         `${prefix}${isLeft ? "│   " : "    "}`,
         false
       );
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└──> " : "┌──> "}${node.data}`);
     if (node.left !== null) {
-      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      this.#prettyPrint(
+        node.left,
+        `${prefix}${isLeft ? "    " : "│   "}`,
+        true
+      );
+    }
+  }
+
+  get viewTree() {
+    this.#prettyPrint(this.#root);
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    let reqdNode = this.#root;
+
+    while (1) {
+      console.log(reqdNode);
+      if (newNode.data > reqdNode.data) {
+        if (reqdNode.right === null) {
+          reqdNode.right = newNode;
+          break;
+        }
+        reqdNode = reqdNode.right;
+      } else {
+        if (reqdNode.left === null) {
+          reqdNode.left = newNode;
+          break;
+        }
+        reqdNode = reqdNode.left;
+      }
     }
   }
 }
 
-let bin = new BinarySearchTree();
-let root = bin.buildTree([1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345], 0, 10);
-console.log(bin.prettyPrint(root));
-// console.log(root);
+let bin = new BinarySearchTree([1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]);
+bin.viewTree;
+
+bin.insert(6);
+bin.viewTree;
